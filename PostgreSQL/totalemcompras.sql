83864e3fc38cb39c368be156affefaf926138739
MatheusSanteago <matheussanteago@gmail.com>
2023-03-26 13:37:55 -0300

    Adicionando busca de gastos por cliente

--- PostgreSQL/totalemcompras.sql
+++ PostgreSQL/totalemcompras.sql
@@ -1,13 +1,8 @@
-select cliente, sum(valortotal) as totalemcompras from relacional.vendas as vendas
-inner join relacional.clientes as c on (vendas.idcliente = c.idcliente)
-inner join relacional.itensvenda as it on (vendas.idvenda = it.idvenda)
-inner join relacional.produtos as p on (it.idproduto = p.idproduto)
-group by cliente
-order by totalemcompras ASC;
+/* Listar o total gasto por cada cliente */
 
 select cliente, sum(valortotal) as totalemcompras from relacional.vendas as vendas
 inner join relacional.clientes as c on (vendas.idcliente = c.idcliente)
 inner join relacional.itensvenda as it on (vendas.idvenda = it.idvenda)
 inner join relacional.produtos as p on (it.idproduto = p.idproduto)
 group by cliente
-order by totalemcompras DES;
+order by totalemcompras desc;
